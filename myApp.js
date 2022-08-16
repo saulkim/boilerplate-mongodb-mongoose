8377add835393291dfe65e368e5888ac5a5c4b10
saulkim <>
2022-08-15 18:40:03 -0700

    all next commits are attempts to get fcc test to pass

--- myApp.js
+++ myApp.js
@@ -100,8 +100,11 @@ const removeById = (personId, done) => {
 const removeManyPeople = (done) => {
     const nameToRemove = "Mary";
     Person.deleteMany({name: nameToRemove}, (err, data)=>{
+	if(err){return console.log(err)}
+	else {
 	return done(null, data);
-    })
+	}
+	})
 
 };
 
